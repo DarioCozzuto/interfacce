2d0fcbe2f6334e66ec1b11f0d1db2e964fdf1e33
DarioCozzuto <77857380+DarioCozzuto@users.noreply.github.com>
2021-02-15 01:18:15 +0100

    Add files via upload

---
+++ interfacce.php
@@ -0,0 +1,48 @@
+<?php
+//Interfaces are declared with the interface keyword
+//To implement an interface, a class must use the implements keyword
+//Una classe atratta non può essere istanziata
+
+//Interfaces vs. Abstract Classes
+//-Interfaces cannot have properties, while abstract classes can
+//-All interface methods must be public, while abstract class methods is public or protected
+//-All methods in an interface are abstract
+//-Classes can implement an interface while inheriting from another class at the same time
+
+
+
+// Interface definition
+interface Animal {
+    public function makeSound();
+  }
+  
+  // Class definitions
+  class Cat implements Animal {
+    public function makeSound() {
+      echo " Meow ";
+    }
+  }
+  
+  class Dog implements Animal {
+    public function makeSound() {
+      echo " Bark ";
+    }
+  }
+  
+  class Mouse implements Animal {
+    public function makeSound() {
+      echo " Squeak ";
+    }
+  }
+  
+  // Create a list of animals
+  $cat = new Cat();
+  $dog = new Dog();
+  $mouse = new Mouse();
+  $animals = array($cat, $dog, $mouse);
+  
+  // Tell the animals to make a sound
+  foreach($animals as $animal) {
+    $animal->makeSound();
+  }
+  ?>
